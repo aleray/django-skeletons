e252b086c77cc54006d46031c34ad5b578798a49
Alexandre Leray <alexandre@alexandreleray.com>
2010-02-13 03:40:33 +0100

    Uncommented admin and admindoc urls

--- skeleton/urls.py
+++ skeleton/urls.py
@@ -7,11 +7,11 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     # Example:
     # (r'^skeleton/', include('skeleton.foo.urls')),
-
+    
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    
     # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    (r'^admin/', include(admin.site.urls)),
 )
