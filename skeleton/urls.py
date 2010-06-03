85f5c3701f33b120544428adf8cee01ab080f0c8
Stephanie Vilayphiou <stephanie@stdin.fr>
2010-06-03 11:38:49 +0200

    urls: import settings + locale url

--- skeleton/urls.py
+++ skeleton/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -6,15 +7,9 @@ admin.autodiscover()
 
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^skeleton/', include('skeleton.foo.urls')),
-    
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    
-    # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
+    (r'^i18n/', include('django.conf.urls.i18n')),    
 )
 
 
