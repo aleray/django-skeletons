30d2235f50a4ffe21cde27416c344dd33c2431f6
Stephanie Vilayphiou <stephanie@stdin.fr>
2010-06-03 11:35:32 +0200

    load markup app

--- skeleton/settings.py
+++ skeleton/settings.py
@@ -95,6 +95,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.admin',
     'django.contrib.admindocs',
+    'django.contrib.markup',
 )
 
 # Override the server-derived value of SCRIPT_NAME 
